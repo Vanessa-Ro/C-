7580e79731dece4d953a17367722810cd1d7e0a4
Vanessa Rodrigues <71795806+Vanessa-Ro@users.noreply.github.com>
2021-06-29 18:16:30 -0300

    Estrutura Condicional

---
+++ Questionário - Estrutura Condicional/If-Else.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1 = 10;
+            double p2 = 8.5;
+            double media = 0;
+            string desempenho;
+
+            media = (p1 + p2) / 2;
+
+            if (media <= 5)
+                desempenho = "INSATISFATÓRIO :(";
+           
+            else if (media <= 7)
+                desempenho = "REGULAR :|";
+           
+            else if (media <= 8.5)
+                desempenho = "BOM!";
+            else
+                desempenho = "ÓTIMO!!!";
+
+            Console.WriteLine("O desempenho do aluno foi: " + desempenho);
+
+        }
+    }
+}
---
+++ Questionário - Estrutura Condicional/Switch Case.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            switch (opcao)
+            {
+                case "sim":
+                    Console.WriteLine("Você escolheu a opção SIM");
+                    break;
+
+                case "não":
+                case "talvez":
+                    Console.WriteLine("Você não escolheu a opção SIM");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção Inválida!");
+                    break;
+            }
+        }
+    }
+}
