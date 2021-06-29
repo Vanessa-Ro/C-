f541cb4ea73be27391733526009614f2cfbdb80c
Vanessa Rodrigues <71795806+Vanessa-Ro@users.noreply.github.com>
2021-06-29 17:26:28 -0300

    Questionário - Operadores

---
+++ Questionário - Operadores/Ex2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int x = 8, y = 6;
+
+			if (x == y)
+				Console.WriteLine("Os operandos são iguais.");
+			else
+				Console.WriteLine("Os operandos são diferentes.");
+
+			if (x != y)
+				Console.WriteLine("Os operandos são diferentes.");
+			else
+				Console.WriteLine("Os operandos são iguais.");
+
+			if (x > y)
+				Console.WriteLine("O primeiro operando é maior que o segundo operando.");
+			else
+				Console.WriteLine("O segundo operando é maior que o primeiro operando.");
+		}
+    }
+}
---
+++ Questionário - Operadores/Ex3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int media = 10;
+			int presenca = 98;
+
+			if ((media >= 6) && (presenca >= 75))
+				Console.WriteLine("Aprovado!!!");
+			else
+				Console.WriteLine("Reprovado :(");
+
+
+			string desempenho = "ótimo";
+
+			if ((desempenho == "ótimo") || (desempenho == "bom"))
+				Console.WriteLine("Aprovado!!!");
+			else
+				Console.WriteLine("Reprovado :(");
+
+
+			int nota = 6;
+
+			if (!(nota == 10))
+				Console.WriteLine("A nota do aluno(a) foi: 10");
+			else
+				Console.WriteLine("A nota do aluno(a) foi: 6");
+		}
+    }
+}
