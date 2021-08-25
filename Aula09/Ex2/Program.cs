02a5aafdaeb350f85fa7f4978b8a879b9c78c324
Vanessa Rodrigues <71795806+Vanessa-Ro@users.noreply.github.com>
2021-08-24 22:17:45 -0300

    POO - Classes, Métodos e Atributos

---
+++ Aula09/Ex2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta ct1 = new Conta();
+            Conta ct2 = new Conta();
+            Conta ct3 = new Conta();
+
+            Cliente c1 = new Cliente();
+            Cliente c2 = new Cliente();
+            Cliente c3 = new Cliente();
+
+            ct1.setAgencia("1212");
+            ct1.setNumero(212);
+            ct1.setSaldo(100.00f);
+
+            c1.setNome("Vanessa");
+            c1.setSobrenome("Cardoso");
+            c1.setCpf("111.111.111-10");
+            c1.setConta(ct1);
+
+            Console.WriteLine("Informações do Cliente " + c1.getCpf());
+            Console.WriteLine("Nome: " + c1.getNome());
+            Console.WriteLine("Sobrenome: " + c1.getSobrenome());
+            Console.WriteLine("CPF: " + c1.getCpf());
+
+            Console.WriteLine("\nInformações do Conta " + ct1.getAgencia());
+            Console.WriteLine("Agência: " + ct1.getAgencia());
+            Console.WriteLine("Número: " + ct1.getNumero());
+            Console.WriteLine("Saldo: " + ct1.getSaldo());
+
+            ct2.setAgencia("2121");
+            ct2.setNumero(122);
+            ct2.setSaldo(150.00f);
+
+            c2.setNome("Melyssa");
+            c2.setSobrenome("Almeida");
+            c2.setCpf("222.222.222-20");
+            c2.setConta(ct2);
+
+            Console.WriteLine("\nInformações do Cliente " + c2.getCpf());
+            Console.WriteLine("Nome: " + c2.getNome());
+            Console.WriteLine("Sobrenome: " + c2.getSobrenome());
+            Console.WriteLine("CPF: " + c2.getCpf());
+
+            Console.WriteLine("\nInformações do Conta " + ct2.getAgencia());
+            Console.WriteLine("Agência: " + ct2.getAgencia());
+            Console.WriteLine("Número: " + ct2.getNumero());
+            Console.WriteLine("Saldo: " + ct2.getSaldo());
+
+            ct3.setAgencia("4242");
+            ct3.setNumero(144);
+            ct3.setSaldo(175.00f);
+
+            c3.setNome("Luana");
+            c3.setSobrenome("Fernandes");
+            c3.setCpf("333.333.333-30");
+            c3.setConta(ct3);
+
+            Console.WriteLine("\nInformações do Cliente " + c3.getCpf());
+            Console.WriteLine("Nome: " + c3.getNome());
+            Console.WriteLine("Sobrenome: " + c3.getSobrenome());
+            Console.WriteLine("CPF: " + c3.getCpf());
+
+            Console.WriteLine("\nInformações do Conta " + ct3.getAgencia());
+            Console.WriteLine("Agência: " + ct3.getAgencia());
+            Console.WriteLine("Número: " + ct3.getNumero());
+            Console.WriteLine("Saldo: " + ct3.getSaldo());
+
+        }
+    }
+}
---
+++ Aula09/Ex4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Postagem p1 = new Postagem();
+            Postagem p2 = new Postagem();
+            Postagem p3 = new Postagem();
+
+            Usuario u1 = new Usuario();
+
+            p1.Titulo = "Viagem para Itália";
+            p1.Data = "18/06/2025";
+            p1.Conteudo = "Próximo lugar que desejo visitar!";
+            p1.Visitas = 420;
+
+            p2.Titulo = "Viagem para Holanda";
+            p2.Data = "18/06/2026";
+            p2.Conteudo = "Próximo lugar que desejo visitar, após viajar para Itália!";
+            p2.Visitas = 360;
+
+            p3.Titulo = "Viagem para Irlanda";
+            p3.Data = "18/06/2027";
+            p3.Conteudo = "Próximo lugar que desejo visitar, após viajar para Holanda!";
+            p3.Visitas = 480;
+
+            u1.Nome = "Vanessa";
+            u1.Sobrenome = "Cardoso";
+            u1.Senha = "Viagens888";
+            u1.Postagens[0] = p1;
+            u1.Postagens[1] = p2;
+            u1.Postagens[2] = p3;
+
+            Console.WriteLine("\nInformações do Usuário " + u1.Nome);
+            Console.WriteLine("Nome: " + u1.Nome);
+            Console.WriteLine("Sobrenome: " + u1.Sobrenome);
+            Console.WriteLine("Senha: " + u1.Senha);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("\nPost " + (i + 1));
+                Console.WriteLine("Título: " + u1.Postagens[i].Titulo);
+                Console.WriteLine("Data: " + u1.Postagens[i].Data);
+                Console.WriteLine("Conteudo: " + u1.Postagens[i].Conteudo);
+                Console.WriteLine("Visitas: " + u1.Postagens[i].Visitas);
+            }
+        }
+    }
+}
