5759568ea38a7de1634875e7d1032145e63fa586
Vanessa Rodrigues <71795806+Vanessa-Ro@users.noreply.github.com>
2021-08-31 20:16:16 -0300

    Atividade Aula 10

---
+++ Aula10/Gato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex
+{
+    class Gato : Mamifero
+    {
+        public Gato()
+        {
+            Console.Write(" -> Gato\n");
+        }
+
+    }
+}
